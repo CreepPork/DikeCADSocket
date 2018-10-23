c546da92919f602a854dad8e51e1cd10633b9450
Ralfs <creepporkstudios@gmail.com>
2018-10-23 18:21:47 +0300

    Replaced POST requests with proper WebSockets

--- DikeCADSocketServer/DikeCADSocketServer.cs
+++ DikeCADSocketServer/DikeCADSocketServer.cs
@@ -1,15 +1,37 @@
 using System;
+using System.Collections.Generic;
 using System.Threading;
 using CitizenFX.Core;
-using RestSharp;
+using Fleck;
 
 namespace DikeCADSocketServer
 {
     public class DikeCADSocketServer : BaseScript
     {
+        // Socket, isOpen
+        private static readonly Dictionary<IWebSocketConnection, bool> Sockets = new Dictionary<IWebSocketConnection, bool>();
+
         public DikeCADSocketServer()
         {
             EventHandlers.Add("dike:receiveData", new Action<string>(OnReceiveData));
+
+            CreateWebSocketServer();
+        }
+
+        private static void CreateWebSocketServer()
+        {
+            WebSocketServer server = new WebSocketServer("ws://127.0.0.1:8080");
+            
+            server.Start(socket =>
+            {
+                socket.OnOpen += () =>
+                {
+                    socket.Send("Connection established.");
+                    Debug.WriteLine("Socket opened!");
+                    
+                    Sockets.Add(socket, true);
+                };
+            });
         }
 
         private static void OnReceiveData(string json)
@@ -18,22 +40,15 @@ namespace DikeCADSocketServer
 
             new Thread(() =>
             {
-                SendPostRequest(json);
+                foreach (KeyValuePair<IWebSocketConnection,bool> socket in Sockets)
+                {
+                    // If socket is open
+                    if (socket.Value)
+                    {
+                        socket.Key.Send(json);
+                    }
+                }
             }).Start();
-        }
-
-        private static void SendPostRequest(string json)
-        {
-            RestClient client = new RestClient("http://dikecad.garkaklis.com");
-            
-            RestRequest request = new RestRequest("map", Method.POST);
-
-            request.AddParameter("text/json", json, ParameterType.RequestBody);
-
-            IRestResponse response = client.Execute(request);
-            
-            if (! response.IsSuccessful)
-                Debug.WriteLine($"Failed to send data! {json}");
         }
     }
 }
